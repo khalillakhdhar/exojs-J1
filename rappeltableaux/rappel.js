85ab97dbfbb2c2a4f28e188c4aec82a1f97aff0f
khalil <khalillakhdharatc@gmail.com>
2022-06-13 16:13:48 +0100

    Create rappel.js

---
+++ rappeltableaux/rappel.js
@@ -0,0 +1,12 @@
+var tab1=[1,2,3,6,12]; // declaration 1
+var names=new Array("user1","user2"); // declaration 2
+console.log("méthode1");
+for(let el of tab1)
+{
+    console.log(el);
+}
+console.log("méthode2");
+for(let i=0;i<names.length;i++)
+{
+    console.log(names[i]);
+}
